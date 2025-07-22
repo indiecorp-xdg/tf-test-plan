fb755218c4648889d0110e56e171aba4dc6c2217
stephennwachukwu <stephennwac007@gmail.com>
2025-07-22 14:31:38 +0100

    workflow auth fix

--- backend.tf
+++ backend.tf
@@ -4,5 +4,16 @@ terraform {
     storage_account_name = "satflabstore"
     container_name       = "satflabstoreblob001"
     key                  = "terraform.tfstate"
+
+    use_oidc             = true
+    tenant_id            = var.azure_tenant_id # Reference the tenant ID from a variable or directly
+    # client_id          = var.azure_client_id # Optional, as `azure/login` typically sets this
+    # subscription_id    = var.azure_subscription_id
   }
+}
+
+
+variable "azure_tenant_id" {
+  description = "The Azure Tenant ID for OIDC authentication."
+  type        = string
 }
